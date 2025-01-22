753f8cdb97b58ebbe70f52a8db134a87e0e27547
mrpsycho-dev <stinkmang6@gmail.com>
2025-01-23 02:13:52 +0500

    switch plater logic 2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const board = document.querySelector('.board');
 const stateText = document.querySelector('.state-text');
-const boardState = ['', '', '', '', '', '', '', '', ''];
+let boardState = ['', '', '', '', '', '', '', '', ''];
 
 let currentPlayer, winState;
 const init = function () {
@@ -13,6 +13,11 @@ init();
 
 const switchPlayer = function () {
   currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  if (currentPlayer === 'X') {
+    stateText.textContent = `Player X's turn`;
+  } else if (currentPlayer === 'O') {
+    stateText.textContent = `Player O's turn`;
+  }
 };
 
 const gameLogic = function () {
@@ -32,5 +37,23 @@ gameLogic();
 
 const handleCellClick = function () {};
 
-const checkWin = function () {};
+const winPositions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+const checkWin = function () {
+  winPositions.forEach(function alpha() {
+    if (winPositions.every(winPositions) === boardState) {
+      console.log(true);
+    } else {
+      console.log(`game logic sucks`);
+    }
+  });
+};
 const checkDraw = function () {};
