8e25def3ae52ae5779de295c0a0afc754237db25
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-03-08 03:34:19 +0500

    i did it i guess

--- script.js
+++ script.js
@@ -2,40 +2,9 @@
 
 const board = document.querySelector('.board');
 const stateText = document.querySelector('.state-text');
+
+let currentPlayer = 'X';
 let boardState = ['', '', '', '', '', '', '', '', ''];
-
-let currentPlayer, winState;
-const init = function () {
-  currentPlayer = 'X';
-  winState = false;
-};
-init();
-
-const switchPlayer = function () {
-  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-  if (currentPlayer === 'X') {
-    stateText.textContent = `Player X's turn`;
-  } else if (currentPlayer === 'O') {
-    stateText.textContent = `Player O's turn`;
-  }
-};
-
-const gameLogic = function () {
-  for (let i = 0; i < 9; i++) {
-    let value = boardState[i];
-    let cells = document.createElement(`div`);
-    cells.innerText = value;
-    board.appendChild(cells);
-    // cells.classList.add('cell');
-    cells.addEventListener('click', function () {
-      cells.innerText = currentPlayer;
-      switchPlayer();
-    });
-  }
-};
-gameLogic();
-
-const handleCellClick = function () {};
 
 const winPositions = [
   [0, 1, 2],
@@ -47,13 +16,41 @@ const winPositions = [
   [0, 4, 8],
   [2, 4, 6],
 ];
-const checkWin = function () {
-  winPositions.forEach(function alpha() {
-    if (winPositions.every(winPositions) === boardState) {
-      console.log(true);
-    } else {
-      console.log(`game logic sucks`);
+
+function createBoard() {
+  for (let i = 0; i < 9; i++) {
+    const cell = document.createElement('div');
+    board.appendChild(cell);
+    cell.classList.add('cell');
+
+    cell.addEventListener('click', function () {
+      if (boardState[i] !== '') return;
+
+      boardState[i] = currentPlayer;
+      cell.innerText = currentPlayer;
+
+      if (checkWin()) {
+        stateText.textContent = `Player ${currentPlayer} Wins !`;
+        return;
+      }
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+      stateText.textContent = `Player ${currentPlayer}'s Turn !`;
+    });
+  }
+}
+createBoard();
+
+function checkWin() {
+  for (let combo of winPositions) {
+    let [a, b, c] = combo;
+
+    if (
+      boardState[a] !== '' &&
+      boardState[a] === boardState[b] &&
+      boardState[b] === boardState[c]
+    ) {
+      return true;
     }
-  });
-};
-const checkDraw = function () {};
+  }
+  return false;
+}
